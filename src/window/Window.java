ce8d8e5f6c58d720859325edf4ed81287a539ad3
Qichao Hong <SuperG@QICHAO.local>
2016-10-26 11:27:49 -0400

    window update

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,5 +1,24 @@
 package window;
 
-public class Window {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Window {
+	public Window(int w, int h, String title, Game game, Handler handler) {
+		game.setPreferredSize(new Dimension(w,h));
+		game.setMaximumSize(new Dimension(w,h));
+		game.setMinimumSize(new Dimension(w,h));
+		
+		JFrame frame = new JFrame(title);
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		game.start(frame);
+	}
 }
